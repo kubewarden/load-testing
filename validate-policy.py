3bbfe4fb77ab62299034a6189211e153e90cb196
José Guilherme Vanz <jguilhermevanz@suse.com>
2021-10-22 11:27:48 -0300

    More accept, reject and mutate requests.
    Adds more tasks to get accept, reject and mutate request over
    three different policies.

--- validate-policy.py
+++ validate-policy.py
@@ -19,7 +19,8 @@ class AdmissionRequest:
 
     def data(self):
         req_uid = uuid.uuid4()
-        return self._template.substitute(req_uid=req_uid)
+        obj_uid = uuid.uuid4()
+        return self._template.substitute(req_uid=req_uid, obj_uid=obj_uid)
 
     def eval(self, user):
         user.client.post(self.endpoint, data=self.data())
@@ -29,4 +30,7 @@ class MyUser(FastHttpUser):
     tasks = [
         AdmissionRequest(endpoint="psp-capabilities", template_file="requests/req_pod_without_security_context.json.tmpl").eval,
         AdmissionRequest(endpoint="psp-capabilities", template_file="requests/req_pod_with_container_with_capabilities_added.json.tmpl").eval,
+        AdmissionRequest(endpoint="psp-user-group", template_file="requests/policy-user-group-reject-request.json").eval,
+        AdmissionRequest(endpoint="psp-user-group", template_file="requests/policy-user-group-mutate-request.json").eval,
+        AdmissionRequest(endpoint="psp-apparmor", template_file="requests/policy-apparmor-accept-request.json").eval
     ]
